fa620f2b242811f20f60ef383e5db6a350b1e22b
Nabil Hardiansa <nhardiansa@gmail.com>
2025-01-22 16:57:38 +0800

    feat: add more interactive like color for selected feeling and log answer after submit

--- src/pages/suggestion-box.vue
+++ src/pages/suggestion-box.vue
@@ -1,12 +1,24 @@
-<script setup lang="ts">
-	import { useStore } from '@/store';
-	import VitailseLogo from '@/assets/logo.png';
+<script setup>
+	import {ref} from 'vue';
 	useHead({
 		title: 'Kotak Saran',
 	});
 
-	const store = useStore();
-	const { t } = useI18n();
+	const selectedOption = ref('');
+	const textareaContent = ref('');
+	const submittedData = ref(null);
+
+	const submitForm = () => {
+		if (selectedOption.value && textareaContent.value) {
+
+			submittedData.value = {
+				selectedOption: selectedOption.value,
+				textareaContent: textareaContent.value
+			};
+
+			console.log('Form submitted:', submittedData.value);
+		};
+	};
 </script>
 
 <template>
@@ -18,27 +30,27 @@
 						Feedback
 					</h1>
 				</div>
-				<form class="form flex flex-col gap-y-9">
+				<form class="form flex flex-col gap-y-9" @submit.prevent="submitForm">
 					<div class="feeling-input flex flex-col gap-y-4">
 						<label for="feeling">Perasaanmu setelah ikut KEY by YukNgaji Makassar</label>
 						<div class="emoticon-radio-buttons flex gap-x-4">
 							<div class="emot-radio-wrapper relative w-max">
-								<div class="emoticon px-[0.3rem] py-2 bg-[#D9D9D9] w-max rounded-full text-3xl">
+								<div class="emoticon px-[0.3rem] py-2 w-max rounded-full text-3xl" :class="selectedOption === 'happy' ? 'bg-[#8E8E93]' : 'bg-[#D9D9D9]' ">
 									😊
 								</div>
-								<input type="radio" value="happy" name="feeling" id="feeling-1" class="emot-radio absolute top-[1px] left-[0.7px] w-[50px] h-[50px] hover:cursor-pointer opacity-0">
+								<input v-model="selectedOption" type="radio" value="happy" name="feeling" id="feeling-1" class="emot-radio absolute top-[1px] left-[0.7px] w-[50px] h-[50px] hover:cursor-pointer opacity-0">
 							</div>
 							<div class="emot-radio-wrapper relative w-max">
-								<div class="emoticon px-[0.3rem] py-2 bg-[#D9D9D9] w-max rounded-full text-3xl">
+								<div class="emoticon px-[0.3rem] py-2 w-max rounded-full text-3xl" :class="selectedOption === 'neutral' ? 'bg-[#8E8E93]' : 'bg-[#D9D9D9]' ">
 									😐
 								</div>
-								<input type="radio" value="neutral" name="feeling" id="feeling-2" class="emot-radio absolute top-[1px] left-[0.7px] w-[50px] h-[50px] hover:cursor-pointer opacity-0">
+								<input v-model="selectedOption" type="radio" value="neutral" name="feeling" id="feeling-2" class="emot-radio absolute top-[1px] left-[0.7px] w-[50px] h-[50px] hover:cursor-pointer opacity-0">
 							</div>
 							<div class="emot-radio-wrapper relative w-max">
-								<div class="emoticon px-[0.3rem] py-2 bg-[#D9D9D9] w-max rounded-full text-3xl">
+								<div class="emoticon px-[0.3rem] py-2 w-max rounded-full text-3xl" :class="selectedOption === 'unhappy' ? 'bg-[#8E8E93]' : 'bg-[#D9D9D9]' ">
 									😒
 								</div>
-								<input type="radio" value="unhappy" name="feeling" id="feeling-3" class="emot-radio absolute top-[1px] left-[0.7px] w-[50px] h-[50px] hover:cursor-pointer opacity-0">
+								<input v-model="selectedOption" type="radio" value="unhappy" name="feeling" id="feeling-3" class="emot-radio absolute top-[1px] left-[0.7px] w-[50px] h-[50px] hover:cursor-pointer opacity-0">
 							</div>
 						</div>
 					</div>
@@ -46,8 +58,8 @@
 						<label for="feedback-area">
 							Kalau kamu punya feedback feel free ya dikirim disini hehe
 						</label>
-						<textarea name="feedback" id="feedback-area" placeholder="Kalau menurutku.........." class="rounded-md border border-[#E4E4E7] min-h-[220px]"></textarea>
-						<button class="text-sm text-white bg-[#18181B] hover:bg-[#27272A] py-2 rounded-md duration-200">
+						<textarea v-model="textareaContent" name="feedback" id="feedback-area" placeholder="Kalau menurutku.........." class="rounded-md border border-[#E4E4E7] min-h-[220px]"></textarea>
+						<button type="submit" class="text-sm text-white bg-[#18181B] hover:bg-[#27272A] py-2 rounded-md duration-200">
 							Kirim
 						</button>
 					</div>
